9b57fe53a35287c6e58b83cce6c986498e5858ca
Albireo <kappa7194@hotmail.it>
2014-04-05 22:14:30 +0200

    Add network and broadcast computation

---
+++ Albireo.IPAddressExtensions/IPAddressExtensions.cs
@@ -0,0 +1,98 @@
+﻿namespace Albireo.IPAddressExtensions
+{
+    using System;
+    using System.Diagnostics.Contracts;
+    using System.Linq;
+    using System.Net;
+
+    public static class IPAddressExtensions
+    {
+        public static IPAddress GetNetworkAddress(
+            this IPAddress address,
+            byte maskBits)
+        {
+            Contract.Requires<ArgumentNullException>(address != null);
+            Contract.Ensures(Contract.Result<IPAddress>() != null);
+
+            return
+                IPAddressExtensions.GetNetworkAddress(
+                    address,
+                    IPAddressExtensions.BitsToMask(maskBits));
+        }
+
+        public static IPAddress GetNetworkAddress(
+            this IPAddress address,
+            IPAddress mask)
+        {
+            Contract.Requires<ArgumentNullException>(address != null);
+            Contract.Requires<ArgumentNullException>(mask != null);
+            Contract.Ensures(Contract.Result<IPAddress>() != null);
+
+            return
+                TransformAddressWithMask(
+                    address,
+                    mask,
+                    (a, m) => (byte) (a & m));
+        }
+        public static IPAddress GetBroadcastAddress(
+            this IPAddress address,
+            byte maskBits)
+        {
+            Contract.Requires<ArgumentNullException>(address != null);
+            Contract.Ensures(Contract.Result<IPAddress>() != null);
+
+            return
+                IPAddressExtensions.GetBroadcastAddress(
+                    address,
+                    IPAddressExtensions.BitsToMask(maskBits));
+        }
+
+        public static IPAddress GetBroadcastAddress(
+            this IPAddress address,
+            IPAddress mask)
+        {
+            Contract.Requires<ArgumentNullException>(address != null);
+            Contract.Requires<ArgumentNullException>(mask != null);
+            Contract.Ensures(Contract.Result<IPAddress>() != null);
+
+            return
+                TransformAddressWithMask(
+                    address,
+                    mask,
+                    (a, m) => (byte) (a | m ^ 255));
+        }
+
+        public static IPAddress BitsToMask(byte bits)
+        {
+            Contract.Ensures(Contract.Result<IPAddress>() != null);
+
+            var mask = string.Empty.PadRight(bits, '1').PadRight(32, '0');
+            var maskValue = Convert.ToInt32(mask, 2);
+            var maskBytes = BitConverter.GetBytes(maskValue).Reverse().ToArray();
+
+            return new IPAddress(maskBytes);
+        }
+
+        private static IPAddress TransformAddressWithMask(
+            IPAddress address,
+            IPAddress mask,
+            Func<byte, byte, byte> transformation)
+        {
+            Contract.Requires<ArgumentNullException>(address != null);
+            Contract.Requires<ArgumentNullException>(mask != null);
+            Contract.Requires<ArgumentNullException>(transformation != null);
+            Contract.Ensures(Contract.Result<IPAddress>() != null);
+
+            var addressBytes = address.GetAddressBytes();
+            var maskBytes = mask.GetAddressBytes();
+            var outputBytes = new byte[addressBytes.Length];
+
+            for (int i = 0, j = outputBytes.Length; i < j; i++)
+            {
+                outputBytes[i] = transformation(addressBytes[i], maskBytes[i]);
+            }
+
+            return new IPAddress(outputBytes);
+        }
+    }
+}
