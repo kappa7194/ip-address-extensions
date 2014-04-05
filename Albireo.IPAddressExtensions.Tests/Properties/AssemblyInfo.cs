f6d1ed466f415f31e35d1e460376314211552889
Albireo <kappa7194@hotmail.it>
2014-04-06 01:12:39 +0200

    Add tests project

---
+++ Albireo.IPAddressExtensions.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Albireo.IPAddressExtensions.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Albireo")]
+[assembly: AssemblyProduct("Albireo.IPAddressExtensions.Tests")]
+[assembly: AssemblyCopyright("Copyright © Albireo 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("dd29f417-f8bc-4436-adcd-9597ac42bcb7")]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
