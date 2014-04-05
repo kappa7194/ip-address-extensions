ec4cef32ab2b649b6b13a24038ed94f197557668
Albireo <kappa7194@hotmail.it>
2014-04-06 01:13:35 +0200

    Add first four reserved networks tests

---
+++ Albireo.IPAddressExtensions.Tests/IsReservedAddressTests/ReservedNetwork1Tests.cs
@@ -0,0 +1,11 @@
+﻿namespace Albireo.IPAddressExtensions.Tests.IsReservedAddressTests
+{
+    using System.Net;
+
+    public class ReservedNetwork1Tests : ReservedNetworkTestsBase
+    {
+        public ReservedNetwork1Tests() : base(null, "0.0.0.0", "0.255.255.255", "1.0.0.0")
+        {
+        }
+    }
+}
---
+++ Albireo.IPAddressExtensions.Tests/IsReservedAddressTests/ReservedNetwork3Tests.cs
@@ -0,0 +1,11 @@
+﻿namespace Albireo.IPAddressExtensions.Tests.IsReservedAddressTests
+{
+    using System.Net;
+
+    public class ReservedNetwork3Tests : ReservedNetworkTestsBase
+    {
+        public ReservedNetwork3Tests() : base("100.63.255.255", "100.64.0.0", "100.127.255.255", "100.128.0.0")
+        {
+        }
+    }
+}
