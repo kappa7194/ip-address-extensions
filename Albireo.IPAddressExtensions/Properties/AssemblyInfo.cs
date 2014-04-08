5e781c279666b74798204c521124f7cc3d5abd13
Albireo <kappa7194@hotmail.it>
2014-04-08 22:23:12 +0200

    Bump version number

--- Albireo.IPAddressExtensions/Properties/AssemblyInfo.cs
+++ Albireo.IPAddressExtensions/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9d30cd6f-9291-4cfb-a4fd-1624dd8a39d3")]
-[assembly: AssemblyVersion("0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
